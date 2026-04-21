05cf44fe99207958633befaf18373e3fbd5eddff
baycura <121875749+baycura@users.noreply.github.com>
2026-04-22 00:17:10 +0300

    add src/pages/staff/OrdersPage.jsx

---
+++ src/pages/staff/OrdersPage.jsx
@@ -0,0 +1,3 @@
+export default function OrdersPage() {
+  return <div style={{padding:32,color:"#F0EDE8",fontFamily:"'Barlow Condensed',sans-serif",fontSize:18}}>OrdersPage - Yükleniyor...</div>;
+}
